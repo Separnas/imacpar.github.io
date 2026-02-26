201dec550cc05b46c5c786a8ff8fa5c0daec82a6
Separnas <70232654+Separnas@users.noreply.github.com>
2026-02-26 21:32:39 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true
-  }
-}
+const isProd = process.env.NODE_ENV === 'production';
 
-export default nextConfig
+const nextConfig = {
+  output: 'export',          // ser till att Next exporterar statiska filer
+  images: { unoptimized: true }, // krävs på GitHub Pages (ingen image-optimering)
+  basePath: isProd ? '/imacpar.github.io' : '',   // rätt basväg för project pages
+  assetPrefix: isProd ? '/imacpar.github.io/' : '', // gör att CSS/JS/Bilder hittas
+  trailingSlash: true        // genererar .../index.html för varje sida
+};
+
+export default nextConfig;
